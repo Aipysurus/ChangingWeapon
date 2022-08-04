cb1cffc23777803f9e5987ddda0c6ce6cfa4cbae
Aipysurus <54886633+Aipysurus@users.noreply.github.com>
2022-08-03 17:04:34 -0700

    Navmesh baked. Boss movement working

--- Assets/BossAI.cs
+++ Assets/BossAI.cs
@@ -1,51 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BossAI : MonoBehaviour
 {
     [SerializeField]
-    private GameObject Player;
-    [SerializeField]
-    private float MinDist = 4.0f;
-    [SerializeField]
-    private float MoveSpeed = 4.0f;
+    private Transform player;
+    private NavMeshAgent agent;
+    public float MoveDist = 4.0f;
+    public float AttackDist = 0.7f;
+    public float MoveSpeed = 4.0f;
     public Action_Manage bossActions;
     private bool animationCD = false;
     public float cdTime = 4.0f;
-    Transform pTransform;
     // Start is called before the first frame update
     void Start()
     {
         bossActions = gameObject.GetComponent<Action_Manage>();
-        
+        player = GameObject.FindWithTag("Player").transform;
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Vector3.Distance(transform.position, Player.transform.position));
         if (!animationCD)
         {
             animationCD = true;
-            if (Vector3.Distance(transform.position, Player.transform.position) >= MinDist)
+            agent.SetDestination(player.transform.position);
+            if (Vector3.Distance(transform.position, player.position) >= MoveDist)
             {
                 bossActions.Pressed_run();
-                MoveToPlayer();
             }
-            else
+            else if (Vector3.Distance(transform.position, player.position) >= AttackDist)
             {
-                Debug.Log("Not Moving");
+                gameObject.GetComponent<NavMeshAgent>().velocity = Vector3.zero;
+                bossActions.Pressed_attack_01();
             }
             StartCoroutine(changeMode());
         }
-    }
-
-    private void MoveToPlayer()
-    {
-        transform.LookAt(Player.transform);
-        transform.position += transform.forward * MoveSpeed * Time.deltaTime;
-        
     }
 
     private void randomActions()
