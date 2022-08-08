6ae83d5bbe65e7f9ffc6ed8655fcf51d796e5a2e
Bkhuu149 <bhkhuu@uci.edu>
2022-08-08 11:10:15 -0700

    Worked on health, health regens now and can press k to take damage

--- Assets/HealthScript.cs
+++ Assets/HealthScript.cs
@@ -6,15 +6,21 @@ using UnityEngine.UI;
 public class HealthScript : MonoBehaviour
 {
 
-    public Slider slider;
+    public Slider HealthSlider;
+    public Slider RegenSlider;
+
     public int MaxHealth = 100;
+    public int RegenMaxHealth;
     public int CurrentHealth;
+
+    public float RegenTimer = 1f;
     
 
     // Start is called before the first frame update
     void Awake()
     {
         CurrentHealth = MaxHealth;
+        RegenMaxHealth = MaxHealth;
         SetMaxHealth(MaxHealth);
     }
 
@@ -23,19 +29,44 @@ public class HealthScript : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.K))
         {
-            slider.value -= 10;
+            TakeDamage(25);
+        }
+
+        if (RegenTimer <= 0 && CurrentHealth < RegenMaxHealth )
+        {
+            HealthSlider.value += 1;
+            CurrentHealth += 1;
+            RegenTimer = 1f; // Make this higher for longer regen effect
+        }
+        else
+        {
+            RegenTimer -= Time.deltaTime;
         }
     }
 
     public void SetHealth(int health)
     {
-        slider.value = health;
+        HealthSlider.value = health;
     }
 
     public void SetMaxHealth(int health)
     {
-        slider.maxValue = health;
-        slider.value = health;
+        HealthSlider.maxValue = health;
+        HealthSlider.value = health;
+    }
+
+    public void TakeDamage(int Damage)
+    {
+        int RegenDamage = (int)(Damage / 2);
+
+        RegenSlider.value = HealthSlider.value;
+
+        HealthSlider.value -= Damage;
+        RegenSlider.value -= RegenDamage;
+        CurrentHealth = (int) HealthSlider.value;
+        RegenMaxHealth = (int) RegenSlider.value;
+
+
     }
 
 }
