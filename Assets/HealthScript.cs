f661452e36fea832b7a26c136716c829e9000423
Bkhuu149 <bhkhuu@uci.edu>
2022-08-09 11:25:01 -0700

    Added a way of healing via potions, K to take damage, J to heal, will change keybinds later

--- Assets/HealthScript.cs
+++ Assets/HealthScript.cs
@@ -8,20 +8,29 @@ public class HealthScript : MonoBehaviour
 
     public Slider HealthSlider;
     public Slider RegenSlider;
+    public Text PotionsCounterDisplay; 
 
     public int MaxHealth = 100;
     public int RegenMaxHealth;
     public int CurrentHealth;
 
-    public float RegenTimer = 1f;
+    public float RegenTimerMax = 1f;
+    private float RegenTimer;
+
+    public int PotionAmmount = 3;
+    public int PotionHealing = 25;
+
     
 
     // Start is called before the first frame update
     void Awake()
     {
+        PotionsCounterDisplay.text = "Potions: " + PotionAmmount;
         CurrentHealth = MaxHealth;
         RegenMaxHealth = MaxHealth;
+        RegenTimer = RegenTimerMax;
         SetMaxHealth(MaxHealth);
+
     }
 
     // Update is called once per frame
@@ -34,15 +43,25 @@ public class HealthScript : MonoBehaviour
 
         if (RegenTimer <= 0 && CurrentHealth < RegenMaxHealth )
         {
-            HealthSlider.value += 1;
-            CurrentHealth += 1;
-            RegenTimer = 1f; // Make this higher for longer regen effect
+            Regenerate();
+            RegenTimer = RegenTimerMax;
         }
         else
         {
             RegenTimer -= Time.deltaTime;
         }
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            AddHealth(PotionHealing);
+        }
     }
+
+    public void Regenerate()
+    {
+        HealthSlider.value += 1;
+        CurrentHealth += 1;
+    }
+
 
     public void SetHealth(int health)
     {
@@ -55,18 +74,37 @@ public class HealthScript : MonoBehaviour
         HealthSlider.value = health;
     }
 
-    public void TakeDamage(int Damage)
+    public void TakeDamage(int damage)
     {
-        int RegenDamage = (int)(Damage / 2);
+        
+        int regenDamage = (int)(damage / 2);
 
         RegenSlider.value = HealthSlider.value;
 
-        HealthSlider.value -= Damage;
-        RegenSlider.value -= RegenDamage;
+        HealthSlider.value -= damage;
+        RegenSlider.value -= regenDamage;
         CurrentHealth = (int) HealthSlider.value;
         RegenMaxHealth = (int) RegenSlider.value;
+    }
 
+    public void AddHealth(int health)
+    {
 
+        if (PotionAmmount > 0)
+        {
+            CurrentHealth += health;
+
+            if (CurrentHealth > MaxHealth)
+            {
+                CurrentHealth = MaxHealth;
+            }
+
+            RegenMaxHealth = CurrentHealth;
+            HealthSlider.value = CurrentHealth;
+            RegenSlider.value = RegenMaxHealth;
+            PotionAmmount -= 1;
+            PotionsCounterDisplay.text = "Potions: " + PotionAmmount;
+        }
     }
 
 }
